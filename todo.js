0be047763083dd8cb7161ea5aee483558c7b9c0f
magarin <mario213@nate.com>
2021-09-19 21:23:59 +0900

    list add remove

---
+++ todo.js
@@ -0,0 +1,39 @@
+const toDoFrom = document.querySelector(".js-toDoForm")
+const toDoInput = toDoFrom.querySelector("input")
+const toDoList = document.querySelector(".js-toDoList")
+
+
+const TODOS_LS = "toDos"
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button")
+  delBtn.innerText = "❌";
+  const span = document.createElement("span")
+  span.innerText = text
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue)
+  toDoInput.value = "";
+}
+
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS)
+  if(toDos !== null) {
+
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoFrom.addEventListener("submit", handleSubmit)
+}
+
+init()
