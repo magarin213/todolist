b5bfd62080b8cdecdf784291281814ed5dbd0538
magarin <mario213@nate.com>
2021-09-19 19:08:52 +0900

    투두리스트

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText =`${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  const sethours = hours - 12;
+  const amOrpm = hours >= 12 ? "pm" : "am"
+  clockTitle.innerText = amOrpm + ` ${hours < 10 ? `${hours}` : hours > 12 ? `0${sethours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
