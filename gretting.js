ed948f720dc22221e320ea7ac2c9d62c627f4619
magarin <mario213@nate.com>
2021-09-19 21:04:59 +0900

    localstorage에 key value

--- gretting.js
+++ gretting.js
@@ -5,6 +5,21 @@ const greeting = document.querySelector(".js-greeting")
 const USER_LS = "currentUser"
 const SHOWING_CN = "showing"
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text)
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue)
+  saveName(currentValue)
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN)
+  form.addEventListener("submit", handleSubmit)
+}
 
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN)
@@ -15,7 +30,7 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS)
   if(currentUser === null){
-
+    askForName()
   }else{
     paintGreeting(currentUser)
   }
